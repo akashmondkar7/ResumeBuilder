ce3c2a2a10641cd75564781b0b49bb087525d27c
Akash <akashmondkar0303@gmail.com>
2026-04-20 01:12:15 +0530

    hero and nav bar done, added tailwind and react router dom, added google font, added banner and hero components, added routes to app.jsx, added styling to home page and layout page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Banner from '../components/Home/Banner.jsx'
+import Hero from '../components/Home/Hero.jsx'
 
 const Home = () => {
   return (
     <div>
-      <h1>Home page</h1>
+
+      <Banner />
+      <Hero />
+
     </div>
   )
 }
