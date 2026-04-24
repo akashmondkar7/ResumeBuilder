d8641adece710d6f0b8b047ba1530e99c4323408
Akash <akashmondkar03@gmail.com>
2026-04-25 01:33:50 +0530

    dashboard.jsx

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { LucidePlus } from 'lucide-react'
+import { LucidePlus, LucideUpload, LucideUploadCloud } from 'lucide-react'
 import React from 'react'
 const Dashboard = () => {
   return (
@@ -6,8 +6,14 @@ const Dashboard = () => {
       <div className='max-w-7xl mx-auto px-4 py-8'>
         <p className='text-2xl font-medium mb-6 bg-gradient-to-r from-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden'>Welcome, Joe Doe</p>
         <div className='flex gap-4'>
-          <button>
-            <LucidePlus size={16} className='inline mr-2' />
+          <button className='w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-indigo-500 hover:shadow-lg transition-all duration-300 cursor-pointer'>
+            <LucidePlus size={16} className='size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-indigo-300 to indigo-500 text-white rounded-full  ' />
+            <p className='text-sm group-hover:text-purple-600 transition-all duration-300'>Create Resume</p>
+
+          </button>
+           <button className='w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-indigo-500 hover:shadow-lg transition-all duration-300 cursor-pointer'>
+            <LucideUploadCloud size={16} className='size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-purple-300 to purple-500 text-white rounded-full  ' />
+            <p className='text-sm group-hover:text-purple-600 transition-all duration-300'>Upload Existing</p>
 
           </button>
         </div>
