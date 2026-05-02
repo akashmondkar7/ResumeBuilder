c7d14aab15606e8c6c74763725c82d605c9e0b82
Akash <akashmondkar03@gmail.com>
2026-05-02 19:24:51 +0530

    server setup

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Server is Live...");
+});
+
+// Server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
