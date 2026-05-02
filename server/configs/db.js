db8ff18ee505d1e69af10fa5f45e6b7bb598d5cd
Akash <akashmondkar03@gmail.com>
2026-05-02 20:52:23 +0530

    userSchema

---
+++ server/configs/db.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    let mongodbURI = process.env.MONGODB_URI;
+    const projectName = "resume-builder";
+
+    if (!mongodbURI) {
+      throw new Error("MONGODB_URI environment variable not set");
+    }
+
+    // Remove trailing slash if exists
+    if (mongodbURI.endsWith("/")) {
+      mongodbURI = mongodbURI.slice(0, -1);
+    }
+
+    await mongoose.connect(`${mongodbURI}/${projectName}`);
+
+    mongoose.connection.on("connected", () => {
+      console.log("Connected to MongoDB");
+    });
+
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1); // optional but recommended
+  }
+};
+
+export default connectDB;
