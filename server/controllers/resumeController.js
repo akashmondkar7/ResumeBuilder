3f31f8b5757122be6fc1dae71c90145dda91b5c6
Akash <akashmondkar03@gmail.com>
2026-05-03 20:05:15 +0530

    added fuctions

--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -1,4 +1,67 @@
-
+import Resume from "../models/Resume.js";
 
 //controller for creating a new resume
+
 //post: /api/resume/create
+export const createResume = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { title } = req.body;
+
+    //create new resume
+    const newResume = await Resume.create({ userId, title });
+
+    // return success message
+    return res
+      .status(201)
+      .json({ message: "Resume created successfully", Resume: newResume });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+//controller for delating a resume
+// Delete: /api/resume/delete
+
+export const deleteResume = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { resumeId } = req.params;
+
+    await Resume.findOneAndDelete({ userID, _id: resumeId });
+
+    //return success massage
+
+    return res.status(200).json({ message: "Resume deleted successfully" });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+// get user resume by id
+// GET: /api/resumes/get
+
+export const getResumeById = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { resumeId } = req.params;
+
+    const resume = await Resume.findOne({ userID, _id: resumeId });
+
+    if (!resume) {
+      return res.status(404).json({ message: "Resume not found" });
+    }
+    //return success massage
+    resume.__v = undefined;
+    resume.createdAt = undefined;
+    resume.updatedAt = undefined;
+    return res.status(200).json({resume });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+//get resume by id public 
+// GET: /api/resumes/public
+
+
