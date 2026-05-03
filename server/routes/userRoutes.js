bdfec2716ec0dedf290f1366fdb472d7e8f2b4fe
Akash <akashmondkar03@gmail.com>
2026-05-03 12:20:52 +0530

    added UserRoutes

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { getUserById, LoginUser, registerUser } from "../controllers/userController.js";
+import protect from "../middlewares/authMiddleware.js";
+
+
+const userRouter = express.Router();
+
+
+userRouter.post('/register',registerUser)
+userRouter.post('/login', LoginUser)
+userRouter.get('/data',protect, getUserById)
