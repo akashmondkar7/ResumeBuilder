382d9d61a0172c720f551a80dbb9217947ca0fed
Akash <akashmondkar03@gmail.com>
2026-05-10 21:14:45 +0530

    ...

--- client/src/components/Home/Navbar.jsx
+++ client/src/components/Home/Navbar.jsx
@@ -9,18 +9,18 @@ const Navbar = () => {
     const navigate = useNavigate()
     const logoutUser = () => {
         navigate('/')
-        dispatch(logout)
+        dispatch(logout())
 
     }
 
   return (
     <div className='shadow bg-white'>
-        <nav className='flex items-center justify-between max-w-7xl mx-auto px py-3.5 text-slate-800 transition-all'>
+        <nav className='flex items-center justify-between max-w-7xl mx-auto px-4 py-3.5 text-slate-800 transition-all'>
             <Link to="/">
             <img src="/logo.svg" alt=" Logo" className="w-auto h-11" />
             </Link>
             <div className=' flex items-center justify-center gap-4'>
-                <p>Hi,{user.name}</p>
+                <p>Hi,{user?.name}</p>
                 <button onClick={logoutUser} className=' bg-white hover:bg-slate-50 border border-gray-300 px-7 py-1.5 rounded-full active:scale-95 transition-all'>Logout</button>
             </div>
         </nav>
