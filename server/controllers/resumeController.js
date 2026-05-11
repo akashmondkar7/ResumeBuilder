6f621447598cb19c11cdf8c55d85ca3a35a45cc4
Akash <akashmondkar03@gmail.com>
2026-05-11 21:01:38 +0530

    ..

--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -88,40 +88,44 @@ export const getPublicResumeById = async (req, res) => {
 // controller for  udpadting resume
 // PUT: /api/resumes/update
 
-export const updateResume = async (req, res) =>{
-    try {
-        const userId = req.userId;
-        const {resumeId, resumeData, removeBackground} = req.body
-        const image = req.file;
-        
-        let resumeDataCopy; 
-        if(typeof resumeData === 'string'){
-            resumeDataCopy = await JSON.parse(resumeData)
-        }else{
-            resumeDataCopy = structuredClone(resumeData)
-        }
+export const updateResume = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { resumeId, resumeData, removeBackground } = req.body;
+    const image = req.file;
 
-        if(image){
-            
-
-            const imageBufferData = fs.createReadStream(image.path)
-
-            const response = await imagekit.files.upload({
-                            file: imageBufferData,
-                            fileName: 'resume.png',
-                            folder: 'user-resumes',
-                             transformation: {
-                                pre: 'w-300,h-300,fo-face,z-0.75' + (removeBackground ? ',e-bgremove' : '')
-                             }
-                            });
-
-            resumeDataCopy.personal_info.image = response.url
-        }
-
-       const resume = await Resume.findByIdAndUpdate({userId, _id: resumeId}, resumeDataCopy, {new: true})
-
-       return res.status(200).json({message: 'Saved successfully', resume})
-    } catch (error) {
-        return res.status(400).json({message: error.message})
+    let resumeDataCopy;
+    if (typeof resumeData === "string") {
+      resumeDataCopy = await JSON.parse(resumeData);
+    } else {
+      resumeDataCopy = structuredClone(resumeData);
     }
-}
+
+    if (image) {
+      const imageBufferData = fs.createReadStream(image.path);
+
+      const response = await imagekit.files.upload({
+        file: imageBufferData,
+        fileName: "resume.png",
+        folder: "user-resumes",
+        transformation: {
+          pre:
+            "w-300,h-300,fo-face,z-0.75" +
+            (removeBackground ? ",e-bgremove" : ""),
+        },
+      });
+
+      resumeDataCopy.personal_info.image = response.url;
+    }
+
+    const resume = await Resume.findByIdAndUpdate(
+      { userId, _id: resumeId },
+      resumeDataCopy,
+      { new: true },
+    );
+
+    return res.status(200).json({ message: "Saved successfully", resume });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
