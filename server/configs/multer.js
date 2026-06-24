6486e88da2fc436c3943627adf0bc549a1a6d582
Akash <akashmondkar03@gmail.com>
2026-06-24 21:29:40 +0530

    fixed multer.js

--- server/configs/multer.js
+++ server/configs/multer.js
@@ -12,6 +12,7 @@ const storage = multer.diskStorage({
     cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
+    
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, uniqueSuffix + "-" + file.originalname);
   },
